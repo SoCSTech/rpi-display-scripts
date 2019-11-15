3a1f932ec22352fb403b016f2d14a17d8a61f5cc
Tom Reed <t.reed1104@gmail.com>
2019-11-15 12:21:42 +0000

    Fleshed out the setup script a bit more

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
+
+## Pull the most recent version of the repo
 git pull
+
+## Update the system
+apt-get update
+apt-get upgrade -y
+
+## Make sure Unclutter is installed, unclutter removes the mouse from the screen in fullscreen
 apt-get install -y unclutter
+
+## Copy the scripts into their locations
 /bin/cp -rf autostart /home/pi/.config/lxsession/LXDE-pi/autostart
 /bin/cp -rf display.sh /home/pi/display/sh
+
+## Give the display script the right privledges
 chmod 777 /home/pi/display.sh
