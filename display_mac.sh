1c6282a7e1727a8e0424725bca4bd26106d77355
Tom Reed <treed@Toms-MacBook-Pro.local>
2020-10-23 17:02:35 +0100

    Fixed and tested the mac script

--- display_mac.sh
+++ display_mac.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pgrep -x "Google Chrome"
 hostname=$(hostname -f)
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --noerrdialogs --disable-infobars --kiosk --app=10.5.11.20/yggdrasil/\#/$hostname --incognito
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --noerrdialogs --disable-infobars --kiosk 10.5.11.20/yggdrasil/\#/$hostname --incognito
