3c12e34ff18a432d867d03a9d17ca6be6595dfbd
Tom Reed <t.reed1104@gmail.com>
2020-11-04 09:49:55 +0000

    Added the bash profile set to the setup script

--- setup_mac.sh
+++ setup_mac.sh
@@ -4,3 +4,4 @@
 chmod +x $HOME/autostart/display_mac.sh
 #TODO - Register on login item list for the user instead of by opening terminal
 echo "$HOME/autostart/display_mac.sh" >> $HOME/.bash_profile
+source $HOME/.bash_profile
