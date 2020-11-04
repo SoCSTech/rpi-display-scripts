9f7ee816368ac0561d9e0779bb09c002f57f4888
Tom Reed <t.reed1104@gmail.com>
2020-11-04 09:42:37 +0000

    Fixed kill command and vue-router #

--- display_mac.sh
+++ display_mac.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-pgrep -x "Google Chrome"
-hostname=$(hostname -f)
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --noerrdialogs --disable-infobars --kiosk 10.5.11.20/yggdrasil/labdispmac --incognito
+killall 'Google Chrome'
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --noerrdialogs --disable-infobars --kiosk 10.5.11.20/yggdrasil/\#/labdispmac --incognito
