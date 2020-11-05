d1a3916f68d6b43d149e739b1339f6c0290b0872
Tom Reed <t.reed1104@gmail.com>
2020-11-05 13:03:13 +0000

    Moved the mac+display sleep timers to before the bash profile changes

--- setup_mac.sh
+++ setup_mac.sh
@@ -3,11 +3,11 @@
 /bin/cp -rf display_mac.sh $HOME/autostart/
 sudo chmod +x $HOME/autostart/display_mac.sh
 
+# Set the Mac and Display sleep timers
+sudo systemsetup -setdisplaysleep Never
+sudo systemsetup -setcomputersleep Never
+
 # Add the display script to the user Bash profile so it triggers when terminal opens
 # TODO - Register on login item list for the user instead of by opening terminal
 echo "$HOME/autostart/display_mac.sh" >> $HOME/.bash_profile
 source $HOME/.bash_profile
-
-# Set the Mac and Display sleep timers
-sudo systemsetup -setdisplaysleep Never
-sudo systemsetup -setcomputersleep Never
